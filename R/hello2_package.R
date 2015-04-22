8b1312812c47fd351ed3fb8ecfdd7dd0835e76aa
sofilibi <sofilibi@gmail.com>
2015-04-21 19:36:16 -0500

    TEST1

---
+++ R/hello2_package.R
@@ -0,0 +1,1 @@
+#' hello2-package#'#' A Good Title#'#' A thorough description#'#' @name hello2-package#' @docType packageNULL  
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(hello2)
+
+test_check("hello2")
